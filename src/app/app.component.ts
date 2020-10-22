656df6846b7596d40411d0262ad1d7b64f8da3b3
Marcus Hakozaki <marcus.hakozaki@gmail.com>
2020-10-21 21:22:56 -0500

    Paginação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,12 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
 
+  //PAGINATION
+  collectionSize;
+  pageSize = 5;
+  page = 1;
+
+
   pokemons: Pokemon[] = [];
   pokemonForm: FormGroup;
 
@@ -34,7 +40,10 @@ export class AppComponent {
           result.forEach(e => {
             this.pokemonService.getByNameOrId(e.name)
               .subscribe(
-                result => this.pokemons.push(result)
+                result => {
+                  this.pokemons.push(result);
+                  this.collectionSize = this.pokemons.length;
+                }
               )
           })
         }
@@ -45,7 +54,7 @@ export class AppComponent {
    * Faz a fuiltragem no array de pokemons
    * @param text 
    */
-  search(text: string) {    
+  search(text: string) {
     this.pokemons = this.pokemons.filter(pokemon => {
       const term = text.toLowerCase();
       return pokemon.name.toLowerCase().includes(term);
@@ -55,13 +64,13 @@ export class AppComponent {
   /**
    * Rotina do botão pesquisar da tabela
    */
-  pesquisar(){    
+  pesquisar() {
     this.search(this.pokemonForm.controls['name'].value)
   }
 
   ngOnInit() {
     this.buildForm();
-    this.getAll();    
+    this.getAll();
   }
 
 }
